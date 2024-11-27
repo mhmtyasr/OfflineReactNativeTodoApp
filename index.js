2050bed222d8d2424ad8e2bf163503df30c0a202
yasir.aktunc <>
2024-11-27 10:39:15 +0300

    For ios modal container fixed and fonts added

--- index.js
+++ index.js
@@ -6,29 +6,9 @@ import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'reflect-metadata';
-// eslint-disable-next-line no-undef
-if (Platform.OS === 'windows') {
-  import('react-native-get-random-values-windows')
-    .then(module => {
-      module.default();
-      startApp();
-    })
-    .catch(error => {
-      startApp();
-    });
-} else {
-  import('react-native-get-random-values')
-    .then(module => {
-      module.default();
-      startApp();
-    })
-    .catch(error => {
-      startApp();
-    });
-}
+import('react-native-get-random-values-windows')
+
 
 LogBox.ignoreAllLogs(true);
 
-function startApp() {
-  AppRegistry.registerComponent(appName, () => App);
-}
+AppRegistry.registerComponent(appName, () => App);
